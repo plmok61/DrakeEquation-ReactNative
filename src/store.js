b5444ef2291288d31dc5a21166d5b314f61d7c39
Phil <plmok61@gmail.com>
2017-02-17 16:59:41 -0600

    customizes launchscreen

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, createStore} from 'redux'
+import { applyMiddleware, createStore } from 'redux'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 import promise from 'redux-promise-middleware'

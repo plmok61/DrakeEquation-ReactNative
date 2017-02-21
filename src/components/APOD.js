8203e93c83468b39f30e9334a43463c9519e4372
Phil <plmok61@gmail.com>
2017-02-21 16:30:38 -0600

    removes react state from APOD and adds support for videos

--- src/components/APOD.js
+++ src/components/APOD.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
-import { View, ActivityIndicator, ScrollView, Dimensions } from 'react-native'
+import { View, ActivityIndicator, ScrollView, Dimensions, WebView } from 'react-native'
 import { getAPOD } from '../actions/apodActions'
 import TextPrimary from './TextPrimary'
 import TextSecondary from './TextSecondary'
@@ -8,72 +8,70 @@ import ScaledImage from './ScaledImage'
 import styles from '../styles'
 
 class APOD extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      animating: true,
-      hdurl: 'http://apod.nasa.gov',
-      title: null,
-      explanation: null,
-      date: null,
-    }
-  }
 
   componentDidMount() {
     console.log('APOD props: ', this.props)
     this.props.getAPOD()
   }
 
-  // getAPOD() {
-  //   const baseURL = 'https://api.nasa.gov/planetary/apod?api_key='
-  //   const apiKey = 'MYsfdOuaFm4HsA7dQpr8dXBtzO7bKz13cXJWwZyc'
-
-  //   axios.get(`${baseURL}${apiKey}`)
-  //     .then((response) => {
-  //       console.log(response.data)
-  //       const { hdurl, title, explanation, date, media_type } = response.data
-  //       if (media_type === 'image') {
-  //         this.setState({
-  //           animating: false,
-  //           hdurl,
-  //           title,
-  //           explanation,
-  //           date,
-  //         })
-  //       }
-  //     })
-  // }
-
   render() {
+    const { fetching, apodData, error } = this.props.apod
+
+    if (apodData.media_type === 'image') {
+      return (
+        <View style={styles.container}>
+          <ScrollView>
+            <ScaledImage
+              uri={apodData.hdurl}
+              width={Dimensions.get('window').width}
+            />
+            <View style={styles.apodText}>
+              <TextPrimary>
+                {apodData.title}
+              </TextPrimary>
+              <TextSecondary>
+                {apodData.explanation}
+              </TextSecondary>
+            </View>
+          </ScrollView>
+        </View>
+      )
+    } else if (apodData.media_type === 'video') {
+      return (
+        <View style={styles.container}>
+          <ScrollView>
+            <WebView
+              style={{height: Dimensions.get('window').width * 0.5625, width: Dimensions.get('window').width}}
+              source={{ uri: apodData.url }}
+            />
+            <View style={styles.apodText}>
+              <TextPrimary>
+                {apodData.title}
+              </TextPrimary>
+              <TextSecondary>
+                {apodData.explanation}
+              </TextSecondary>
+            </View>
+          </ScrollView>
+        </View>
+      )
+    }
     return (
       <View style={styles.container}>
-        {
-          this.state.animating ?
-            <ActivityIndicator
-              animating={this.state.animating}
-              style={[styles.centering, { height: 80 }]}
-              size="large"
-              color="white"
-            />
-          :
-            <ScrollView>
-              <ScaledImage
-                uri={this.state.hdurl}
-                width={Dimensions.get('window').width}
-              />
-              <View style={styles.apodText}>
-                <TextPrimary>
-                  {this.state.title}
-                </TextPrimary>
-                <TextSecondary>
-                  {this.state.explanation}
-                </TextSecondary>
-              </View>
-            </ScrollView>
-        }
+        <ActivityIndicator
+          animating={fetching}
+          style={[styles.centering, { height: 80 }]}
+          size="large"
+          color="white"
+        />
       </View>
     )
   }
+}
+
+APOD.propTypes = {
+  getAPOD: PropTypes.func.isRequired,
+  apod: PropTypes.object,
 }
 
 const mapStateToProps = state => ({

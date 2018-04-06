b85019e15e8a32da55a3c7bb52f2ce94cc1aa076
Phil Mok <plmok61@gmail.com>
2018-04-05 22:45:50 -0500

    animations

--- App.js
+++ App.js
@@ -23,7 +23,6 @@ export default class App extends React.Component {
       'Exo-2-Bold': require('./assets/fonts/Exo2-Bold.ttf'),
     });
     this.setState({ fontsLoaded: true });
-    console.log(this.state);
   }
 
   fontsLoadingComplete() {
--- src/components/Result.js
+++ src/components/Result.js
@@ -11,36 +11,73 @@ function getRandomInt(mini, maxi) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+// function throttle(func, delay = 500) {
+//   let timeout = null;
+//   return function (...args) {
+//     if (!timeout) {
+//       timeout = setTimeout(() => {
+//         func.call(this, ...args);
+//         timeout = null;
+//       }, delay);
+//     }
+//   };
+// }
+
+// const brackets = [10000000000, 1000000000, 100000000, 10000000, 1000000, 100000, 10000, 1000, 100, 10, 1];
+const brackets = [10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000, 10000000000, 100000000000];
+const { width } = Dimensions.get('window');
+
 class Result extends Component {
   constructor(props) {
     super(props);
     this.createOrbiters = this.createOrbiters.bind(this);
+    this.orbiters = [];
   }
 
   createOrbiters() {
-    const count = this.props.numCivs ? Math.ceil(this.props.numCivs / 100000) : 0;
+    let count = 0;
+    const { numCivs } = this.props;
+    brackets.forEach((item) => {
+      if (numCivs < item) {
+        count += (numCivs / (item / 10));
+      } else {
+        count += 10;
+      }
+    });
+
+    const { length } = this.orbiters;
     let i;
-    const orbiters = [];
-    for (i = 0; i < count; i += 1) {
-      orbiters.push(i);
+    if (count > length) {
+      for (i = length; i < count; i += 1) {
+        this.orbiters.push(i);
+      }
+    } else if (count < length) {
+      for (i = length; i >= count; i -= 1) {
+        this.orbiters.pop();
+      }
     }
-    console.log(orbiters);
-    return orbiters;
+
+    return this.orbiters;
   }
 
   render() {
     return (
       <View style={styles.result}>
-        <TextSecondary style={{ fontSize: 25, textAlign: 'center', paddingTop: 40 }}>
+        <TextSecondary
+          onLayout={(event) => {
+            console.log('y', event.nativeEvent.layout);
+          }}
+          style={{ fontSize: 25, textAlign: 'center', marginTop: 20 }}
+        >
           Civilizations in our galaxy:
         </TextSecondary>
         {
           this.createOrbiters().map((i) => {
-            const radius = getRandomInt(1, Dimensions.get('window').width / 2);
+            const radius = getRandomInt(1, width / 2);
             return (
               <OrbiterAnimation
                 key={`orbiter-${i}`}
-                customStyle={{ position: 'absolute', top: 90 }}
+                customStyle={{ position: 'absolute', top: 60 }}
                 size={getRandomInt(1, 10)}
                 duration={5000 + (radius * 50)}
                 color={teal}
@@ -49,7 +86,7 @@ class Result extends Component {
             );
           })
         }
-        <TextSecondary style={{ fontSize: 40 }}>
+        <TextSecondary style={{ fontSize: 40, marginVertical: 20 }}>
           {this.props.numCivs}
         </TextSecondary>
       </View>
